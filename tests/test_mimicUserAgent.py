da2d30e8009180a9091c138958a8f0c6ab3bfa57
kurotom <55354389+kurotom@users.noreply.github.com>
2023-07-06 15:24:20 -0400

    nueva estructura

--- tests/test_mimicUserAgent.py
+++ tests/test_mimicUserAgent.py
@@ -1,5 +1,5 @@
 import unittest
-from src.mimicUserAgent import mimic_user_agent as mua
+from src.mimic_user_agent.mimicUserAgent import mimic_user_agent as mua
 
 
 class TestRandomUserAgent(unittest.TestCase):
